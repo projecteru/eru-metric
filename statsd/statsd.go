7e118e624c967f9b42808f7945bfc8c01825e115
CMGS <ilskdw@gmail.com>
2016-02-22 18:58:01 +0800

    support statsd

---
+++ statsd/statsd.go
@@ -0,0 +1,37 @@
+package statsd
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+
+	statsdlib "github.com/cactus/go-statsd-client/statsd"
+)
+
+func CreateStatsDClient(addr string) *StatsDClient {
+	return &StatsDClient{
+		Addr: addr,
+	}
+}
+
+type StatsDClient struct {
+	sync.Mutex
+	Addr string
+}
+
+func (self *StatsDClient) Close() error {
+	return nil
+}
+
+func (self *StatsDClient) Send(data map[string]float64, endpoint, tag string, timestamp, step int64) error {
+	remote, err := statsdlib.NewClient(self.Addr, "")
+	defer remote.Close()
+	if err != nil {
+		return err
+	}
+	for k, v := range data {
+		key := fmt.Sprintf("%s.%s.%s", endpoint, tag, k)
+		remote.Raw(key, strconv.FormatFloat(v, 'f', -1, 64), 1.0)
+	}
+	return nil
+}
